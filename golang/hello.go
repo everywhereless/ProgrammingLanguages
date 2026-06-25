808c76cece44df435041eef3432e9055a18aa7ef
everywhereless <everywhereless@gmail.com>
2026-06-25 07:33:01 +0530

    first commit

---
+++ golang/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var name string = "Dragonized"
+var number int = 1997
+var isAdult bool = true
+
+var fullName, age = "Anik Mandal", 26
+
+func main() {
+	fmt.Println(name, number, isAdult)
+}
