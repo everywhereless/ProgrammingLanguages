9e4913967ef412b5920ec55f0097fdda0ce92171
everywhereless <everywhereless@gmail.com>
2026-06-25 07:35:57 +0530

    Update hello.go

--- golang/hello.go
+++ golang/hello.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 var name string = "Dragonized"
-var number int = 1997
-var isAdult bool = true
-
-var fullName, age = "Anik Mandal", 26
 
 func main() {
-	fmt.Println(name, number, isAdult)
+	fmt.Println(name)
 }
